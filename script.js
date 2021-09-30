866eed91354c10020eceb923cbc81cd27019c4e1
abeTavarez <abeincwireless@gmail.com>
2021-09-30 10:44:03 -0400

    fetchKami delete method

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const http = new FetchKami;
 
-// Get users
+//* === Get Users
 // http.get(`https://jsonplaceholder.typicode.com/users`)
 // .then(data => console.log(data))
 // .catch(err => console.log(err))
@@ -13,12 +13,17 @@ const data = {
     email: 'abe-dev@gmail.com'
 };
 
-// Create Post
+//* === Create User
 // http.post(`https://jsonplaceholder.typicode.com/users`, data)
 // .then(data => console.log(data))
 // .catch(err => console.log(err))
 
-// Update Post
-http.put(`https://jsonplaceholder.typicode.com/users/3`, data)
+//* === Update User
+// http.put(`https://jsonplaceholder.typicode.com/users/3`, data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+//* === Delete User
+http.delete(`https://jsonplaceholder.typicode.com/users/3`)
 .then(data => console.log(data))
 .catch(err => console.log(err))
