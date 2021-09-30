430196dbbf4331a6687d2eec9eac690177480588
abeTavarez <abeincwireless@gmail.com>
2021-09-29 23:29:54 -0400

    FetchKami class, get method

--- FetchKami.js
+++ FetchKami.js
@@ -1,1 +1,20 @@
+/**
+ * 
+ * FetchKami Library
+ * Library for making HTTP Requests
+ * 
+ * @version 1.0.0
+ * @author Abraham tavarez
+ * @license MIT
+ * 
+ * */
 
+class FetchKami {
+    //* === Make an HTTP GET
+    get(url) {
+        fetch(url) //fetch returns a promise
+        .then(res => res.json()) // mapped it to json
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
+}
